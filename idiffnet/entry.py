a1d4505311ede538fef6cf70fa203b6976d6b938
zhanzl711 <56958362+zhanzl711@users.noreply.github.com>
2022-08-27 14:57:00 +0800

    Add files via upload

---
+++ idiffnet/entry.py
@@ -0,0 +1,53 @@
+'''
+    author: Peijie Sun
+    e-mail: sun.hfut@gmail.com 
+    released date: 04/18/2019
+'''
+
+import sys, os, argparse
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
+
+sys.path.append(os.path.join(os.getcwd(), 'class'))
+
+from ParserConf import ParserConf
+from DataUtil import DataUtil
+from Evaluate import Evaluate
+
+from idiffnet import idiffnet
+
+
+
+def executeTrainModel(config_path, model_name):
+    print(config_path)
+    #print('System start to prepare parser config file...')
+    conf = ParserConf(config_path)
+    conf.parserConf()
+    
+    #print('System start to load TensorFlow graph...')
+    model = eval(model_name)
+    model = model(conf)
+
+    #print('System start to load data...')
+    data = DataUtil(conf)
+    evaluate = Evaluate(conf)
+
+    import train as starter
+    starter.start(conf, data, model, evaluate)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Welcome to the Experiment Platform Entry')
+    parser.add_argument('--data_name', default='yelp', nargs='?', help='data name')
+    parser.add_argument('--model_name', default='idiffnet', nargs='?', help='model name')
+    parser.add_argument('--gpu', default='0', nargs='?', help='available gpu id')
+
+    args = parser.parse_args()
+
+    data_name = args.data_name
+    model_name = args.model_name
+    device_id = args.gpu
+    
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = device_id
+    config_path = os.path.join(os.getcwd(), 'conf/%s_%s.ini' % (data_name, model_name))
+
+    executeTrainModel(config_path, model_name)
